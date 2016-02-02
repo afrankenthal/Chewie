aedecc8e01f84dcab30e299476bd4920bb4b332e
menasce <menasce@10e39ee6-6802-49b7-9bb1-e0472594542b>
2016-02-02 16:32:43 +0000

    Updated to comply with new structure od directories after adding mocFile etc...
    git-svn-id: svn+ssh://cdcvs.fnal.gov/cvs/projects/chewie/trunk@40 10e39ee6-6802-49b7-9bb1-e0472594542b

--- Express/compile.py
+++ Express/compile.py
@@ -10,10 +10,10 @@ def doMakefile():
   i=0
   outFile = open("../MakefileExpress","w")
   for line in makefileContent:
-    if (line.find("src/main.") != -1):
-      makefileContent[i] = line.replace("src/main.","Express/ChewieExpress.");
-    elif (line.find("main.") != -1):
+    if (line.find("main.") != -1):
       makefileContent[i] = line.replace("main.","Express/ChewieExpress.");
+      makefileContent[i] = makefileContent[i].replace("objFiles/Express/","objFiles/");
+      makefileContent[i] = makefileContent[i].replace("src/Express/","Express/");
     if (line.find("QMAKE_TARGET") != -1 or line.find("TARGET") != -1):
       makefileContent[i] = line.replace("Chewie","Express/ChewieExpress");
     outFile.write(makefileContent[i])
@@ -23,7 +23,8 @@ def doMakefile():
 
 def main():
   doMakefile();
-  cmd = "rm -f ChewieExpress.o ChewieExpress; cd ..; qmake; make -f MakefileExpress; cd Express; rm ../MakefileExpress"; 
+#  cmd = "rm -f ChewieExpress.o ChewieExpress; cd ..; qmake; make -f MakefileExpress; cd Express; rm ../MakefileExpress"; 
+  cmd = "rm -f ChewieExpress.o ChewieExpress; cd ..; qmake; make -f MakefileExpress; cd Express; "; 
   proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE);
   out,err = proc.communicate();
   print out
